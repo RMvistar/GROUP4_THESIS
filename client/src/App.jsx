91b93e707056a7d0d6fad0cde16fa4fbd8f39de9
Daniel Jess Reynoso <danieljess.reynoso@gmail.com>
2025-12-12 13:36:58 +0800

    Updated NavigationBar.css. Reorganized component files into subdirectories.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import NavigationBar from './components/NavigationBar.jsx';
-import Dashboard from './components/Dashboard.jsx';
-import NodeDetails from './components/NodeDetails.jsx';
-import Alerts from './components/Alerts.jsx';
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import NavigationBar from "./components/NavigationBar.jsx";
+import Dashboard from "./components/Dashboard/Dashboard.jsx";
+import NodeDetails from "./components/NodeDetails/NodeDetails.jsx";
+import Alerts from "./components/Alerts/Alerts.jsx";
+import "./App.css";
 
 function App() {
   return (
     <Router>
       <NavigationBar />
       <div className="body-container">
-        
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/node-details" element={<NodeDetails />} />
@@ -19,7 +17,7 @@ function App() {
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/NodeDetails/NodeDetails.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NodeContainer from "./NodeContainer.jsx";
+import "./NodeDetails.css";
+
+function NodeDetails() {
+  return (
+    <div className="node-details">
+      <NodeContainer />
+    </div>
+  );
+}
+
+export default NodeDetails;
